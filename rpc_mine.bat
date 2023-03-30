1bf8c0f7c133a1076f5e2b073077d20e06aff482
Tkz007 <127329112+Tkz007@users.noreply.github.com>
2023-03-30 14:41:29 -0300

    Add files via upload

---
+++ rpc_mine.bat
@@ -0,0 +1,7 @@
+:loop
+
+"""D:\andyr\mineria\dero\astrominer.exe""" -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmx0s95v4r6r2tspwsydr -r dero.friendspool.club:10300 -p rpc
+
+timeout /t 5
+
+goto loop
